7c6a60356af5b8d1e0ff7d0e69d96b0d61f3d15b
miedb23 <mikael.edberger@orebro.se>
2016-02-29 15:08:01 +0100

    Återställt menyändringar

--- nyaorebro.se/HEAD/modal.js
+++ nyaorebro.se/HEAD/modal.js
@@ -17,7 +17,8 @@
 		init($(this));                     		
 								
 		$("a[href*='"+ options.ref +"']").click(function() {
-			add_block_page();
+			//add_block_page();
+            $('.or-backdrop').show();
 			add_popup_box();
 			add_styles();
 			
@@ -36,14 +37,15 @@
 		}
 		
 		function add_block_page(){
-			var block_page = $('<div class="or-backdrop"></div>');						
+			var block_page = $('<div class="or-backdrop" style="display:none"></div>');						
 			$(block_page).appendTo('body');
 		}
 		 		
 		 function add_popup_box(){			 			                                                             			 			 
 			 $('.or-modal-close').click(function(){
 				$(this).parent().fadeOut().hide();                         
-				$('.or-backdrop').fadeOut().remove();				 
+				$('.or-backdrop').fadeOut().hide();		
+                return false;		 
 			 });
 		}
 
@@ -52,6 +54,7 @@
          $('.or-modal-content').wrap('<div class="or-modal" style="display:none;"></div>');
          $('.or-modal-content').before('<a href="#" class="or-modal-close"><i class="fa fa-close"></i> Stäng</a>');
          $(selector).show();   
+         add_block_page();
       }
 		
 	};
