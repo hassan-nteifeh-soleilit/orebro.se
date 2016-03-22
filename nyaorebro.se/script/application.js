0e621035b5aceea68dd0ebf6cf1788770dc8a86f
miedb23 <mikael.edberger@orebro.se>
2016-03-22 11:03:36 +0100

    Nyhetslistning

--- nyaorebro.se/script/application.js
+++ nyaorebro.se/script/application.js
@@ -81,6 +81,11 @@
         $(".or-tree .or-toggle-panel").click(function() {
             $(this).parent().toggleClass("or-expanded");
         });
+		
+		/* Expandera filter items */
+        $(".or-news-filter .or-toggle-panel").click(function() {
+            $(this).parent().toggleClass("or-expanded");
+        });
 
     });
 
