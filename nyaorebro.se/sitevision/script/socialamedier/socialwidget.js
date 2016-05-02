a816daa7431d51a2c3ec3582d91218f33bd1d099
miedb23 <mikael.edberger@orebro.se>
2016-05-02 09:05:32 +0200

    Social media script

--- nyaorebro.se/sitevision/script/socialamedier/socialwidget.js
+++ nyaorebro.se/sitevision/script/socialamedier/socialwidget.js
@@ -1,33 +1,23 @@
 var propertyUtil = require('PropertyUtil');
 var portletContextUtil = require('PortletContextUtil');
 var currentPage = portletContextUtil.getCurrentPage();
-var instaId = propertyUtil.getString(currentPage,"instagramId");
-var fbId = propertyUtil.getString(currentPage,"facebookId");
-var ytubeId = propertyUtil.getString(currentPage,"youTubeId");
-var instagramToken = propertyUtil.getString(currentPage,"instagramToken");
-
-/* Instagram details */
-var instaId = 'BEdb_r8TFfB';
-var INSTA_TOKEN = '237164884.3723313.4c686780aeb54e6dbb662fb1f301b791';  //May change!!!
-
-/* Facebook details */
-var fbId = '1115029451872819';
-var FB_APP_ID = '678017432303988';
-var FB_SECRET_KEY = 'c3186e14c7bd2c73fcdbc4c64af4c7f7';   
-var FB_PAGE_ID = '126400134069094';
-
-/* YouTube details */
-var yTubeId = 'NE3zC__GGNE';	
-var YTUBE_KEY = 'AIzaSyCZrQ3gyBKsSH7EI6eHztJ5BogNrNnBRvg';
+var instaId = propertyUtil.getString(currentPage,"instaId");
+var fbToken = propertyUtil.getString(currentPage,"fbToken");
+var fbId = propertyUtil.getString(currentPage,"fbId");
+var fbPageId = propertyUtil.getString(currentPage,"fbPageId");
+var yTubeToken = propertyUtil.getString(currentPage,"ytubeToken");
+var yTubeId = propertyUtil.getString(currentPage,"ytubeId");
+var instaToken = propertyUtil.getString(currentPage,"instaToken");
+var instaId = propertyUtil.getString(currentPage,"instaId");
 
 var socialMedias =[];
 var url, connection, data;
 
 /* Instagram */
-if(instagramToken && !instagramToken.isEmpty()) {      
+if(instaToken && !instaToken.isEmpty() && instaId && !instaId.isEmpty()) {      
       try{
                   
-            url = new java.net.URL('https://api.instagram.com/v1/media/shortcode/'+ instaId + '?access_token=' + INSTA_TOKEN );   
+            url = new java.net.URL('https://api.instagram.com/v1/media/shortcode/'+ instaId + '?access_token=' + instaToken );   
 
             connection = url.openConnection();       
             data = JSON.parse(Packages.org.apache.commons.io.IOUtils.toString(connection.getInputStream(), 'UTF-8')).data;            
@@ -45,33 +35,37 @@ if(instagramToken && !instagramToken.isEmpty()) {
 
 }
 
-
 /* Facebook */   	
-url = new java.net.URL('https://graph.facebook.com/v2.6/' + FB_PAGE_ID + '_'+ fbId + '?fields=type,full_picture,caption,description,message,created_time,permalink_url,link,shares,comments.limit(1).summary(true),likes.limit(1).summary(true),reactions.limit(1).summary(true)&access_token=' + FB_APP_ID + '|' + FB_SECRET_KEY );
-	 	
-connection = url.openConnection();   	
-data = JSON.parse(Packages.org.apache.commons.io.IOUtils.toString(connection.getInputStream(), 'UTF-8'));      
+if(fbToken && !fbToken.isEmpty() && fbId && fbPageId && !fbId.isEmpty() && !fbPageId.isEmpty()) {      
+   
+   url = new java.net.URL('https://graph.facebook.com/v2.6/' + fbPageId + '_'+ fbId + '?fields=type,full_picture,caption,description,message,created_time,permalink_url,link,shares,comments.limit(1).summary(true),likes.limit(1).summary(true),reactions.limit(1).summary(true)&access_token=' + fbToken);
 
-var shares = data.hasOwnProperty('shares') ? data.shares.count : 0;
-var pictureUrl = data.hasOwnProperty('full_picture') ? data.full_picture : null;
-followlink = "https://www.facebook.com/" + FB_PAGE_ID;
+   connection = url.openConnection();   	
+   data = JSON.parse(Packages.org.apache.commons.io.IOUtils.toString(connection.getInputStream(), 'UTF-8'));      
 
-socialMedias.push(new socialMedia('FB','',data.message,data.created_time,pictureUrl,data.permalink_url,followlink,data.likes.summary.total_count,data.comments.summary.total_count,shares,0,data.reactions.summary.total_count));
-            
+   var shares = data.hasOwnProperty('shares') ? data.shares.count : 0;
+   var pictureUrl = data.hasOwnProperty('full_picture') ? data.full_picture : null;
+   followlink = "https://www.facebook.com/" + fbPageId;
 
-/* YouTube*/	
-url = new java.net.URL('https://www.googleapis.com/youtube/v3/videos?id=' + yTubeId + '&key=' + YTUBE_KEY + '&part=snippet,statistics');
-	 
-connection = url.openConnection();   	
-data = JSON.parse(Packages.org.apache.commons.io.IOUtils.toString(connection.getInputStream(), 'UTF-8'));   
-data = data.items[0];
-var likes = data.hasOwnProperty('likeCount') ? data.statistics.likeCount : 0;
-var dislikes = data.hasOwnProperty('dislikeCount') ? data.statistics.dislikeCount : 0;
-var comments = data.hasOwnProperty('commentCount') ? data.statistics.commentCount : 0;
-var permalink = 'https://www.youtube.com/'+ yTubeId;
-var followlink = 'https://www.youtube.com/user/'+ data.channelTitle;
+	socialMedias.push(new socialMedia('FB','',data.message,data.created_time,pictureUrl,data.permalink_url,followlink,data.likes.summary.total_count,data.comments.summary.total_count,shares,0,data.reactions.summary.total_count));
+}            
 
-socialMedias.push(new socialMedia('YTUBE',data.snippet.title,data.snippet.description,data.snippet.publishedAt,data.snippet.thumbnails.medium.url,permalink,followlink,likes,comments,0,dislikes,0));
+if(yTubeToken && !yTubeToken.isEmpty() && yTubeId && !yTubeId.isEmpty()) {     
+
+   /* YouTube*/	
+   url = new java.net.URL('https://www.googleapis.com/youtube/v3/videos?id=' + yTubeId + '&key=' + yTubeToken + '&part=snippet,statistics');
+
+   connection = url.openConnection();   	
+   data = JSON.parse(Packages.org.apache.commons.io.IOUtils.toString(connection.getInputStream(), 'UTF-8'));   
+   data = data.items[0];
+   var likes = data.statistics.hasOwnProperty('likeCount') ? data.statistics.likeCount : 0;
+   var dislikes = data.statistics.hasOwnProperty('dislikeCount') ? data.statistics.dislikeCount : 0;
+   var comments = data.statistics.hasOwnProperty('commentCount') ? data.statistics.commentCount : 0;
+   var permalink = 'www.youtube.com/embed/'+ yTubeId + '?autoplay=1';
+   var followlink = 'http://www.youtube.com/user/'+ data.channelTitle;
+
+   socialMedias.push(new socialMedia('YTUBE',data.snippet.title,data.snippet.description,data.snippet.publishedAt,data.snippet.thumbnails.medium.url,permalink,followlink,likes,comments,0,dislikes,0));
+}
 
 function socialMedia(type, title, text, timestamp, thumbnail, permalink, followlink, likes, comments, shares, dislikes, reactions) {	 
    this.type = type;
@@ -80,31 +74,31 @@ function socialMedia(type, title, text, timestamp, thumbnail, permalink, followl
 	this.thumbnail = thumbnail; 
 	this.permalink = permalink; 
    this.followlink = followlink;
-   this.showStats = (likes+ comments+ shares+ dislikes+ reactions) > 0;   
+   this.showStats = (likes+ comments+ shares+ dislikes+ reactions) > 0 || type == 'YTUBE';   
 
    var date;
    switch (type) {
       case 'INSTA':
          this.name = 'Instagram';
          this.timestamp = timestamp;
-   		this.icon ='fa-instagram';
+         this.icon ='fa-instagram';
          date = new Date().setTime(timestamp);
          this.stats = [{icon:'fa-comments', count:comments},{icon:'fa-heart', count:likes}];
          break;
       case 'FB':
          this.name = 'Facebook';
          date =  new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(timestamp);
-   		this.icon ='fa-facebook';
+         this.icon ='fa-facebook';
          this.stats = [{icon:'fa-reactions', count:reactions},{icon:'fa-thumbs-o-up', count:likes}, {icon:'fa-comments', count:comments}, {icon:'fa-share', count:shares}];
          break;               
       case 'YTUBE':
          this.name = 'Youtube';
          date =  new SimpleDateFormat("yyyy-MM-dd'T'HH:mm").parse(timestamp);
    		this.icon ='fa-youtube';
-         this.stats = [{icon:'fa-thumbs-o-up', count:likes},{icon:'fa-thumbs-o-down', count:dislikes},{icon:'fa-comments', count:dislikes}];         
+         this.stats = [{icon:'fa-thumbs-o-up', count:(likes+'')},{icon:'fa-thumbs-o-down', count:(dislikes+'')},{icon:'fa-comments', count:(dislikes+'')}];         
          break;               
       default:
-            break;
+         break;
    }
    
    this.timestamp = new SimpleDateFormat("d MMMM",portletContextUtil.getCurrentLocale()).format(date);
