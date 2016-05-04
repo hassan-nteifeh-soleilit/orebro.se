cf6db9d01104c3bc303f17e084e54457155f4a7e
miedb23 <mikael.edberger@orebro.se>
2016-05-04 09:57:21 +0200

    Kodjustering

--- gymnasieskolorna/script/application.js
+++ gymnasieskolorna/script/application.js
@@ -57,44 +57,44 @@
       });
 	  
 	  
-	          /* Ikoner i dokumentlistningsrutorna */
-        $(".or-related-documents .or-text-content a").each(function(index) {
+			/* Ikoner i dokumentlistningsrutorna */
+			$(".or-related-documents .or-text-content a").each(function(index) {
+				var title = $(this).attr("title");
+				var icon, sizeStr;
 
-            var title = $(this).attr("title");
-            var icon, sizeStr;
+				try {
+					if (title && title.length > 0) {
+							title = title.replace('(', '').replace(')', '');
+							var titleSplit = title.split(',');
+							icon = titleSplit[0];
+							sizeStr = titleSplit[1].replace(' ', '');
+					}
+				} catch (err) {
+					console.log('Error document icons');
+				}
+				
+				if (sizeStr) {
+					switch (icon) {
+						case 'doc':
+							icon = 'word';
+							break;
+						case 'pdf':
+							icon = 'pdf';
+							break;
+						case 'xls':
+							icon = 'excel';
+							break;
+						default:
+							icon = '';
+					}
+					var content = '<i class="fa fa-file-' + icon + '-o"><span>' + sizeStr + '</span></i>'
+					$(this).prepend(content);
+					$(this).next().text('');
+				}
+			});
+		 
+					
 
-
-            try {
-                if (title && title.length > 0) {
-                    title = title.replace('(', '').replace(')', '');
-                    var titleSplit = title.split(',');
-                    icon = titleSplit[0];
-                    sizeStr = titleSplit[1].replace(' ', '');
-                }
-            } catch (err) {}
-
-
-            if (sizeStr) {
-                switch (icon) {
-                    case 'doc':
-                        icon = 'word';
-                        break;
-                    case 'pdf':
-                        icon = 'pdf';
-                        break;
-                    case 'xls':
-                        icon = 'excel';
-                        break;
-                    default:
-                        icon = '';
-                }
-                var content = '<i class="fa fa-file-' + icon + '-o"><span>' + sizeStr + '</span></i>'
-                $(this).prepend(content);
-                $(this).next().text('');
-            }
-
-        });
-   
              
       /* Responsiva tabeller */  
       $(".sv-table-portlet table").stacktable({myClass:'stacktable small-only'});
