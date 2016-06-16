747f36b50d3f445b62c1a48b8a360958bd12bc88
miedb23 <mikael.edberger@orebro.se>
2016-06-16 12:00:04 +0200

    Relaterade dokument ikon

--- gymnasieskolorna/script/application.js
+++ gymnasieskolorna/script/application.js
@@ -43,7 +43,7 @@
         
       /* Ikoner i dokumentlistningsrutorna */
       $(".or-docs-box-content a").each(function( index ) {
-         var title = $( this ).attr("title");
+         var title = $(this).attr("title");
          var iconCss, sizeStr;
          
          try {
@@ -55,50 +55,59 @@
             }            
          } catch(err) {}
          
-         var content = '<div class="fileiconcontainer"><div class="iconfile ' + iconCss + '"></div></div>'         
+         var content = '<div class="fileiconcontainer"><div class="iconfile' + iconCss + '"></div></div>';      
          $(this).before(content);   
          if(sizeStr){
             $(this).next().text( sizeStr );
          }
       
       });
+	  
+	  
+		/* Ikoner i nya dokumentlistningsrutorna */
+		$(".or-related-documents .or-text-content a").each(function(index) {
+			var title = $(this).attr("title");
+			var icon, sizeStr;
+			var openInNewWindow = false;
 
-			/* Ikoner i dokumentlistningsrutorna */
-			$(".or-related-documents .or-text-content a").each(function(index) {
-				var title = $(this).attr("title");
-				var icon, sizeStr;
-
-				try {
-					if (title && title.length > 0) {
-							title = title.replace('(', '').replace(')', '');
-							var titleSplit = title.split(',');
-							icon = titleSplit[0];
-							sizeStr = titleSplit[1].replace(' ', '');
-					}
-				} catch (err) {
-					console.log('Error document icons');
+			try {
+				if (title && title.length > 0) {
+						openInNewWindow = title.indexOf('öppnas i nytt fönster') !== -1;
+						title = title.replace('(', '').replace(')', '').replace('öppnas i nytt fönster', '');							
+						var titleSplit = title.split(',');
+						icon = titleSplit[0];
+						sizeStr = titleSplit[1].replace(' ', '');
+				}
+			} catch (err) {
+				console.log('Error document icons');
+			}
+			
+			if (sizeStr) {
+				switch (icon) {
+					case 'doc':
+						icon = 'word';
+						break;
+					case 'pdf':
+						icon = 'pdf';
+						break;
+					case 'xls':
+						icon = 'excel';
+						break;
+					default:
+						icon = '';
 				}
 				
-				if (sizeStr) {
-					switch (icon) {
-						case 'doc':
-							icon = 'word';
-							break;
-						case 'pdf':
-							icon = 'pdf';
-							break;
-						case 'xls':
-							icon = 'excel';
-							break;
-						default:
-							icon = '';
-					}
-					var content = '<i class="fa fa-file-' + icon + '-o"><span>' + sizeStr + '</span></i>'
-					$(this).prepend(content);
-					$(this).next().text('');
-				}
-			});
+				$(this).prepend('<i class="fa fa-file-' + icon + '-o"><span>' + sizeStr + '</span></i>');									
+				if(openInNewWindow){
+					$(this).append('<i style="font-size: 10px; position:static;" class="fa fa-clone fa-flip-vertical"></i>');				
+				}	
+				$(this).next().text('');
+			}
+		});
+		 
+					
 
+             
       /* Responsiva tabeller */  
       $(".sv-table-portlet table").stacktable({myClass:'stacktable small-only'});
       
