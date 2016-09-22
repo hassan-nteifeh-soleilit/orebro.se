d087cf09e5f51c05f5aad13b800109ad105d4074
miedb23 <mikael.edberger@orebro.se>
2016-09-22 07:53:24 +0200

    Översyn brytpunkter

--- nyaorebro.se/sitevision/script/sidmallar/grundmall/mobilmenyrad/mobilmenyrad.js
+++ nyaorebro.se/sitevision/script/sidmallar/grundmall/mobilmenyrad/mobilmenyrad.js
@@ -1,17 +1,3 @@
-var svUtils = request.getAttribute("sitevision.utils");
-var propertyUtil = svUtils.getPropertyUtil();
-var nodeTypeUtil = svUtils.getNodeTypeUtil();
-var dollar ='$';
-
-//Noden är currentPage 
-var currentPage = svUtils.getPortletContextUtil().getCurrentPage();
-var linkNode = propertyUtil.getNode(currentPage, "menustart");
-
-if (linkNode !== null && nodeTypeUtil.isTypeOf(linkNode, [nodeTypeUtil.SITE_PAGE_TYPE, nodeTypeUtil.PAGE_TYPE] )){   
-   linkNode = propertyUtil.getString(linkNode, "URI");   
-}
-
-/*
 var PropertyUtil = require ('PropertyUtil'),
 	NodeTypeUtil = require('NodeTypeUtil'),
 	PortletContextUtil = require('PortletContextUtil'),	
@@ -21,4 +7,3 @@ var PropertyUtil = require ('PropertyUtil'),
 if (linkNode !== null && NodeTypeUtil.isTypeOf(linkNode, [NodeTypeUtil.SITE_PAGE_TYPE, NodeTypeUtil.PAGE_TYPE] )){   
    linkNode = PropertyUtil.getString(linkNode, "URI");   
 }
-*/
