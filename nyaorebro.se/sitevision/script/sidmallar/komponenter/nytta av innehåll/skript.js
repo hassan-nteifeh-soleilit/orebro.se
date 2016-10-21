ab1106480f62b7364b3080ecd51b7e22dd616bd9
miedb23 <mikael.edberger@orebro.se>
2016-10-21 15:31:44 +0200

    Nytta av innehåll

--- nyaorebro.se/sitevision/script/sidmallar/komponenter/nytta av innehåll/skript.js
+++ nyaorebro.se/sitevision/script/sidmallar/komponenter/nytta av innehåll/skript.js
@@ -1,17 +1,18 @@
 var textarea = request.getParameter("textarea");
 
 
-if(textarea!==null && textarea!==""){
+if(textarea !== null && textarea !== ""){
    sendMailScript(textarea);
 }
 
 
 function sendMailScript(textarea) {
-   var mailBuilder = svUtils.getMailUtil().getMailBuilder();
-   var endecUtil = svUtils.getEndecUtil();
-   var message = textarea;
-   var subject = "Kontakt och tyck till";
-   var email = "feedback-webb@orebro.se";
+   var EndecUtil = require('EndecUtil'),
+       MailBuilder = require('MailUtil').getMailBuilder();
+    
+   var message = textarea,
+       subject = "Kontakt och tyck till",
+       email = "feedback-webb@orebro.se";
  
    //Skapar mailet
    mailBuilder.setSubject(endecUtil.decodeURL(subject));
