675af5a5ae43a57e7dffa3f044ea097135a4fc1f
miedb23 <mikael.edberger@orebro.se>
2016-11-10 14:14:34 +0100

    Delafunktion

---
+++ nyaorebro.se/sitevision/script/sidmallar/grundmall/dela/dela.js
@@ -0,0 +1,7 @@
+var propertyUtil = require('PropertyUtil'),
+	portletContextUtil = require('PortletContextUtil'),
+	resourceLocatorUtil = require('ResourceLocatorUtil'),
+	page = portletContextUtil.getCurrentPage(), 
+	siteName = encodeURIComponent( resourceLocatorUtil.getSite() ),
+	pageName = encodeURIComponent( propertyUtil.getString( page, 'displayName' ) ),
+	url = "www.orebro.se" + encodeURIComponent( propertyUtil.getString( page, 'URI' ) );
