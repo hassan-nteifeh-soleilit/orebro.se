64dbaa5a46150362a73f1af02b2946e6a1feed0f
miedb23 <mikael.edberger@orebro.se>
2016-12-20 09:31:58 +0100

    Relaterade dokument

--- gymnasieskolorna/script/application.js
+++ gymnasieskolorna/script/application.js
@@ -68,31 +68,24 @@
 			/* Ikoner i dokumentlistningsrutorna */
 			$(".or-related-documents .or-text-content a").each(function(index) {
 				var title = $(this).attr("title"),
-					openInNewWindow = false,
 					sizeStr;
-				
-
+					
 				try {
 					if (title && title.length > 0) {
-							openInNewWindow = title.indexOf('öppnas i nytt fönster') !== -1;
-							title = title.replace('(', '').replace(')', '').replace('öppnas i nytt fönster', '');							
-							var titleSplit = title.split(',');							
+							title = title.replace('(', '').replace(')', '').replace('öppnas i nytt fönster','');
+							var titleSplit = title.split(',');						
 							sizeStr = titleSplit[1].replace(' ', '');
 					}
 				} catch (err) {
 					console.log('Error document icons');
 				}
 				
-			
-				if (sizeStr) {					
+				if (sizeStr) {				
 					$(this).find('.or-file-size').text(sizeStr);										
-					if(openInNewWindow){
-						$(this).append('<i style="font-size: 10px; position:static;" class="fa fa-clone fa-flip-vertical"></i>');				
-					}	
 					$(this).next().text('');
 				}
 			});
-		 
+			 
 					
 			/* Expandera meny items */
 			$(".or-tree .or-toggle-panel").click(function() {
