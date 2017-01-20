4aff64019baa4c8b8bd6e013c0a4d332432bac71
miedb23 <mikael.edberger@orebro.se>
2017-01-20 09:56:49 +0100

    Readspeaker https

--- gymnasieskolorna/Sitevision/script/Readspeaker.js
+++ gymnasieskolorna/Sitevision/script/Readspeaker.js
@@ -4,6 +4,6 @@ var PropertyUtil = require('PropertyUtil'),
 
 var pageURL = EndecUtil.encodeURL("www.orebro.se/" + PropertyUtil.getString(PortletContextUtil.getCurrentPage(),'URI'));
 
-var URL ='//app.readspeaker.com/cgi-bin/rsent?customerid=4332&lang=sv_se&url=' + pageURL + '&voice=erik22k&readid=';
+var URL ='//app-eu.readspeaker.com/cgi-bin/rsent?customerid=4332&lang=sv_se&url=' + pageURL + '&voice=erik22k&readid=';
 
 var contentNodeId = PortletContextUtil.getCurrentPortlet().getIdentifier().replace(".", "_");
