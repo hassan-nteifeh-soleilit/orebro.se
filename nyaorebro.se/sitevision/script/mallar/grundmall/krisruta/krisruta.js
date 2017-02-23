80cd3b045db52833e23721917255d25ba79e0d3a
miedb23 <mikael.edberger@orebro.se>
2017-02-23 15:13:21 +0100

    Refakorerat krisrutan
    Använder sig av scriptvariabler istället för hårdkodad arkiv id.

--- nyaorebro.se/sitevision/script/mallar/grundmall/krisruta/krisruta.js
+++ nyaorebro.se/sitevision/script/mallar/grundmall/krisruta/krisruta.js
@@ -1,54 +1,37 @@
-var ARCHIVE_ID = '3.3342eaed15109c3d1d1ec9';
+var LinkRenderer = require('LinkRenderer'),
+    PropertyUtil = require('PropertyUtil'),
+    resourceLocatorUtil = require('ResourceLocatorUtil');
 
-
-var utils = request.getAttribute('sitevision.utils');
-var linkRenderer = utils.getLinkRenderer();
-var scriptUtil = utils.getScriptUtil();
-var propertyUtil = utils.getPropertyUtil();
-var outputUtil = utils.getOutputUtil();
-var resourceLocatorUtil = utils.getResourceLocatorUtil();
-
-
-   
-var archive = resourceLocatorUtil.getNodeByIdentifier(ARCHIVE_ID).getNodes();
-var show = false;
-var smallCss = "";
+var archive = scriptVariables.archiveNode.getNodes(),
+    show = false,
+    smallCss = "";
 
 if(archive && archive.hasNext()) {
    
-   var article = archive.next(); 
-   
-   //out.print(outputUtil.getNodeInfoAsHTML(article,1));
-    
-   
-   var header = propertyUtil.getString(article,'SV.Title','');     
-   var content = propertyUtil.getString(article,'SV.Content','');     
-   var ingress = propertyUtil.getString(article,'SV.Description','');
+   var article = archive.next(),
+       header = PropertyUtil.getString(article,'SV.Title',''),
+       content = PropertyUtil.getString(article,'SV.Content',''),
+       ingress = PropertyUtil.getString(article,'SV.Description','');
   
    if(ingress.isEmpty()){      
       smallCss = "small";
    }
    
-   linkRenderer.update(article, 'or-alert-button ' + smallCss , "MER INFORMATION");
-   
-   
+   LinkRenderer.update(article, 'or-alert-button ' + smallCss , "MER INFORMATION");
 
-   var target = propertyUtil.getNode(article,'alertMessageLink');
-   var link = "";
+   var target = PropertyUtil.getNode(article,'alertMessageLink'),
+       link = "";
          
-                     
    if(target) {
-      linkRenderer.setTarget(target);            
-      var link = linkRenderer.render(); 
+      LinkRenderer.setTarget(target);            
+      link = LinkRenderer.render(); 
    } else {            
-      target = propertyUtil.getString(article,'alertMessageLink');      
+      target = PropertyUtil.getString(article,'alertMessageLink');      
       
       if(target && !target.isEmpty()) {
-         linkRenderer.setStringTarget(target);      
-         var link = linkRenderer.render(); 
+         LinkRenderer.setStringTarget(target);      
+         link = LinkRenderer.render(); 
       }
   }
-       
    show = true;
-
 }
