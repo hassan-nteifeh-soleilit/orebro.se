0b53af07ac44bbff9641c85376ef9cbdc4e8e9dd
miedb23 <mikael.edberger@orebro.se>
2017-02-24 13:34:15 +0100

    Strukturering

--- nyaorebro.se/sitevision/script/mallar/grundmall/mobilmenyrad/mobilmenyrad.js
+++ nyaorebro.se/sitevision/script/mallar/grundmall/mobilmenyrad/mobilmenyrad.js
@@ -1,8 +1,8 @@
 var PropertyUtil = require ('PropertyUtil'),
-	NodeTypeUtil = require('NodeTypeUtil'),
-	PortletContextUtil = require('PortletContextUtil'),	
-	linkNode = PropertyUtil.getNode(PortletContextUtil.getCurrentPage(), "menustart");
-	dollar ='$';
+    NodeTypeUtil = require('NodeTypeUtil'),
+    PortletContextUtil = require('PortletContextUtil'),
+    linkNode = PropertyUtil.getNode(PortletContextUtil.getCurrentPage(), "menustart"),
+    dollar ='$';
 
 if (linkNode !== null && NodeTypeUtil.isTypeOf(linkNode, [NodeTypeUtil.SITE_PAGE_TYPE, NodeTypeUtil.PAGE_TYPE] )){   
    linkNode = PropertyUtil.getString(linkNode, "URI");   
