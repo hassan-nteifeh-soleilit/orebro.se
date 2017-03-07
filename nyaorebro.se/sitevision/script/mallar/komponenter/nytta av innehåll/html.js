0c4819140a1930c88237414ab954638388122dd1
miedb23 <mikael.edberger@orebro.se>
2017-03-07 11:13:36 +0100

    Strukturering

--- nyaorebro.se/sitevision/script/mallar/komponenter/nytta av innehåll/html.js
+++
@@ -1,7 +0,0 @@
-var PropertyUtil = require('PropertyUtil'),
-    PortletContextUtil = require('PortletContextUtil'),
-    currPage = PortletContextUtil.getCurrentPage();
-
-// Fetch the information
-var pageTitle = PropertyUtil.getString(currPage, 'displayName'),
-    url = PropertyUtil.getString(currPage, 'URL', '');  
