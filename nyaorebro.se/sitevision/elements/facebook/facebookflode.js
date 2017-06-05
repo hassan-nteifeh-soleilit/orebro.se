4175aa8f64498499963b9a6a36d485c54096f0d1
alexandralazic <alexandra.lazic@consid.se>
2017-06-05 09:00:54 +0200

    Uppdaterat facebook-flöde
    Fixat så "Följ oss på Facebook"-länken fungerar.

--- nyaorebro.se/sitevision/elements/facebook/facebookflode.js
+++ nyaorebro.se/sitevision/elements/facebook/facebookflode.js
@@ -13,12 +13,12 @@ var requester = require('JsonRequester'),
 	options = {
 		'data' : { 
          'limit': scriptVariables.limit,
-         'fields': 'full_picture,message,link, permalink_url,from,created_time,shares,comments.limit(1).summary(true),likes.limit(1).summary(true),reactions.limit(1).summary(true)',                             
+         'fields': 'full_picture,message,link, permalink_url,from,created_time,shares,comments.limit(1).summary(true),likes.limit(1).summary(true),reactions.limit(1).summary(true),status_type,description,name',                             
          'access_token':  scriptVariables.fbToken
       }   		
    };
 
-	
+		
 
    if(singlePost) {
       url = fbGraphUrl + scriptVariables.fbPageId + '_'+ scriptVariables.fbPostId;     		 
@@ -28,14 +28,39 @@ var requester = require('JsonRequester'),
    }   
 
 
+var followlink = "https://www.facebook.com/" + scriptVariables.fbPageId;
 
 function storePost(item){
       
-      var shares = item.hasOwnProperty('shares') ? item.shares.count : 0;
-      var pictureUrl = item.hasOwnProperty('full_picture') ? item.full_picture : null;
-      followlink = "https://www.facebook.com/" + scriptVariables.fbPageId;
+      var shares = item.hasOwnProperty('shares') ? item.shares.count : 0,
+          pictureUrl = item.hasOwnProperty('full_picture') ? item.full_picture : null,
+          followlink = "https://www.facebook.com/" + scriptVariables.fbPageId,
+          message = item.message;
+   
 
-      posts.push(new socialMedia('FB','',item.message,item.created_time,pictureUrl,item.permalink_url,followlink,item.likes.summary.total_count,item.comments.summary.total_count,shares,0,item.reactions.summary.total_count));               
+   		if(typeof item.message === 'undefined'){
+            message = item.description;
+         }
+   
+   
+   	if(item.status_type === 'created_event') {
+         var event_id = item.id.split('_')[1],
+             start_time;
+                  
+         options.data.fields = '';      
+		         
+         requester.get(fbGraphUrl + event_id,options)
+         	.done(function(json) {            	
+					var date =  new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(json.start_time);
+            	start_time = new SimpleDateFormat("d MMMM",PortletContextUtil.getCurrentLocale()).format(date);
+				});
+         
+                           
+         message = start_time + ' ' + item.name;
+      }
+   		 
+
+      posts.push(new socialMedia('FB','',message,item.created_time,pictureUrl,item.permalink_url,followlink,item.likes.summary.total_count,item.comments.summary.total_count,shares,0,item.reactions.summary.total_count));               
 }
 
 requester.get(url,options)
@@ -63,22 +88,30 @@ requester.get(url,options)
 
 
 function socialMedia(type, title, text, timestamp, thumbnail, permalink, followlink, likes, comments, shares, dislikes, reactions) {	 
+     
    this.type = type;
 	this.title = title; 
-	this.text = text; 	
+	//this.text = text; 	
 	this.thumbnail = thumbnail; 
 	this.permalink = permalink; 
    this.followlink = followlink;
    this.showStats = (likes+ comments+ shares+ dislikes+ reactions) > 0;   
-
+   
    var date;
    
    this.name = 'Facebook';
    date =  new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(timestamp);
    this.icon ='fa-facebook';
    this.stats = [{icon:'fa-reactions', count:reactions},{icon:'fa-comments', count:comments}, {icon:'fa-share', count:shares}];
-      
    
+   //nytt SO
+   if(text !== null || text !== "" ) {
+      this.text = text;
+   } else {
+      this.text = date; 
+   }
+  
+  
    this.timestamp = new SimpleDateFormat("d MMMM",PortletContextUtil.getCurrentLocale()).format(date);
    
 }
