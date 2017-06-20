15af7ffc8ca317756a259dac5748d9a2dbc6416a
Jim van Asperen <jim.van.asperen@soleilit.se>
2017-06-20 16:38:23 +0200

    [SOL-6] Använd defaultvärden för actions som måste skrivas över.

--- nyaorebro.se/sitevision/elements/action/action.js
+++ nyaorebro.se/sitevision/elements/action/action.js
@@ -3,7 +3,15 @@ var LinkRenderer = require('LinkRenderer'),
     PropertyUtil = require('PropertyUtil'),
     MimeTypeUtil = require('MimeTypeUtil');
 
-    
+var ResourceLocatorUtil = require('ResourceLocatorUtil');
+
+/*
+ * Om LinkInternal, LinkDoc eller LinkExternal är lika med sitt defaultvärde används den inte.
+ */
+var DEFAULT_LINK_INTERNAL = ResourceLocatorUtil.getSitePage();
+var DEFAULT_LINK_DOC = ResourceLocatorUtil.getFileRepository();
+var DEFAULT_LINK_EXTERNAL = "*Ej angiven*";
+        
 var url = '<span class="or-elmt-actn-link">...</span>',
     error,
     faIcon,
@@ -19,15 +27,19 @@ if(scriptVariables.IconId === null || scriptVariables.IconId === "" ) {
 
 if (scriptVariables.LinkInternal === null && scriptVariables.LinkDoc === null && (scriptVariables.LinkExternal === null || scriptVariables.LinkExternal === ""))  {  	
    error = "Ställ in extern adress, intern sida <b>eller</b> intern fil";
-   
+}
+else if (scriptVariables.LinkInternal === DEFAULT_LINK_INTERNAL &&
+         scriptVariables.LinkExternal === DEFAULT_LINK_EXTERNAL &&
+         scriptVariables.LinkDoc === DEFAULT_LINK_DOC) {
+   error = "Ställ in extern adress, intern sida <b>eller</b> intern fil";
 } else if (scriptVariables.LinkText === null || scriptVariables.LinkText === ""){
    error = "Länktext saknas";
 } else {
 	//LinkRenderer.setFontClass("or-elmt-action-link");   
    LinkRenderer.setFontClass("or-elmt-actn-link");   
-      
+   LinkRenderer.setOpenNewWindow(scriptVariables.OpenNewWindow);
    
-   if(!(scriptVariables.LinkExternal === null || scriptVariables.LinkExternal === "") ) {         	
+   if (scriptVariables.LinkExternal !== DEFAULT_LINK_EXTERNAL && !(scriptVariables.LinkExternal === null || scriptVariables.LinkExternal === "") ) {         	
          if(scriptVariables.LinkExternal.startsWith("http")){         
          	LinkRenderer.setStringTarget(scriptVariables.LinkExternal);
          }else {
@@ -37,7 +49,7 @@ if (scriptVariables.LinkInternal === null && scriptVariables.LinkDoc === null &&
       	LinkRenderer.setText(scriptVariables.LinkText); 
       	url = LinkRenderer.render();        	
                   
-   } else if(scriptVariables.LinkInternal !== null) {
+   } else if(scriptVariables.LinkInternal !== DEFAULT_LINK_INTERNAL && scriptVariables.LinkInternal !== null) {
        if (LinkRenderer.isValidTarget(scriptVariables.LinkInternal))  {
           LinkRenderer.update(scriptVariables.LinkInternal);                 
           LinkRenderer.setText(scriptVariables.LinkText);                     
@@ -46,7 +58,7 @@ if (scriptVariables.LinkInternal === null && scriptVariables.LinkDoc === null &&
       	 error ="Ogiltig intern sida.";         
        }    
       
-   } else if(scriptVariables.LinkDoc !== null) {
+   } else if(scriptVariables.LinkDoc !== DEFAULT_LINK_DOC && scriptVariables.LinkDoc !== null) {
     	
       var size = PropertyUtil.getDouble(scriptVariables.LinkDoc, 'length',0),
           mime = PropertyUtil.getString(scriptVariables.LinkDoc, 'mimeType', ""),
