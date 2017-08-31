49b79191ba0c25abf50d261c3a1106ed08daff9a
Jim van Asperen <jim.van.asperen@soleilit.se>
2017-08-31 14:18:12 +0200

    [SOL-27] Plusbox - Kontakter.

--- nyaorebro.se/sitevision/dekorationsmallar/plusbox/plusbox.js
+++ nyaorebro.se/sitevision/dekorationsmallar/plusbox/plusbox.js
@@ -1,6 +1,15 @@
 var ResourceLocatorUtil = require('ResourceLocatorUtil'),
     PropertyUtil = require('PropertyUtil');
 
+var interface = {
+    /* Text - Klass för omgivande <div>. Om ej angiven skapas ingen div. */
+    wrapperClass: scriptVariables.wrapperClass,
+    /* Text - Fontawesomeikon, t ex "fa-comments". */
+    icon: scriptVariables.icon,
+    /* Text - Titel, "%s" ersätts med det dekorerades namn. */
+    title: scriptVariables.title
+};
+
 // Returnerar noden som är dekorationsinnehållet.
 var getContentNode = function() {
    var portletId = request.getContext().getCurrentContent();
@@ -13,6 +22,9 @@ try {
    if (contentNode) {
       var contentId = contentNode.getIdentifier().replace(".", "_"),
           contentName = PropertyUtil.getString(contentNode, 'displayName');
+   }
+   if (interface.title && interface.title !== '') {
+        contentName = interface.title.replace("%s", contentName || "").trim();
    }
 } catch (e) {
    out.println("Antingen är sidan som är dekorerad felaktig eller så är kan inte sidan visas i detta läge: " + e);
