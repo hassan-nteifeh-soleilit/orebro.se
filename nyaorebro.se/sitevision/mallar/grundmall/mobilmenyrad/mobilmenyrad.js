77afbb722bf648fdebe77c98850e4edbecd6a9d8
annor72 <ann-sofie.nordling@orebro.se>
2017-09-18 08:49:15 +0200

    [SOL-32] Lagt till metadata för länk från loggan

--- nyaorebro.se/sitevision/mallar/grundmall/mobilmenyrad/mobilmenyrad.js
+++ nyaorebro.se/sitevision/mallar/grundmall/mobilmenyrad/mobilmenyrad.js
@@ -2,8 +2,12 @@ var PropertyUtil = require ('PropertyUtil'),
     NodeTypeUtil = require('NodeTypeUtil'),
     PortletContextUtil = require('PortletContextUtil'),
     linkNode = PropertyUtil.getNode(PortletContextUtil.getCurrentPage(), "menustart"),
+    linkStartNode = PropertyUtil.getNode(PortletContextUtil.getCurrentPage(), "sitestart"),
     dollar ='$';
 
 if (linkNode !== null && NodeTypeUtil.isTypeOf(linkNode, [NodeTypeUtil.SITE_PAGE_TYPE, NodeTypeUtil.PAGE_TYPE] )){   
-   linkNode = PropertyUtil.getString(linkNode, "URI");   
+   linkNode = PropertyUtil.getString(linkNode, "URI");  
+}
+if (linkStartNode !== null && NodeTypeUtil.isTypeOf(linkStartNode, [NodeTypeUtil.SITE_PAGE_TYPE, NodeTypeUtil.PAGE_TYPE] )){   
+   linkStartNode = PropertyUtil.getString(linkStartNode, "URI");  
 }
