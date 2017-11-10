c7d4ed6f0f186d7534b91bce19ec37a17210d48f
annor72 <ann-sofie.nordling@orebro.se>
2017-11-10 11:33:45 +0100

    REQ232077 Tracking code hotjar
    diff i produktion och git

--- nyaorebro.se/script/hojtar.js
+++ nyaorebro.se/script/hojtar.js
@@ -1,7 +1,7 @@
-<!-- Hotjar Tracking Code for http://www.orebro.se -->
-<script>
 (function(h,o,t,j,a,r){
-h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+h.hj=h.hj||function(){
+   (h.hj.q=h.hj.q||[]).push(arguments);
+};
 h._hjSettings={hjid:684573,hjsv:6};
 a=o.getElementsByTagName('head')[0];
 r=o.createElement('script');
@@ -9,4 +9,3 @@ r.async=1;
 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
 a.appendChild(r);
 })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
