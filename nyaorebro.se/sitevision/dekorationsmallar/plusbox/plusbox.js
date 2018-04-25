91562ad8bfe0e6c7eda6f37d99c154e631a4f9b9
Hassan Nteifeh <hassan.nteifeh@soleilit.se>
2018-04-25 16:45:08 +0200

    [SOL-44] Bara den plusbox som öppnades sist kommer att fälla ut när man går in igen på den sida som har den.

--- nyaorebro.se/sitevision/dekorationsmallar/plusbox/plusbox.js
+++ nyaorebro.se/sitevision/dekorationsmallar/plusbox/plusbox.js
@@ -1,31 +1,32 @@
-var ResourceLocatorUtil = require('ResourceLocatorUtil'),
-    PropertyUtil = require('PropertyUtil');
+var ResourceLocatorUtil = require('ResourceLocatorUtil');
+var PropertyUtil = require('PropertyUtil');
+var PortletContextUtil = require('PortletContextUtil');
 
 var interface = {
-    /* Text - Klass för omgivande <div>. Om ej angiven skapas ingen div. */
-    wrapperClass: scriptVariables.wrapperClass,
-    /* Text - Fontawesomeikon, t ex "fa-comments". */
-    icon: scriptVariables.icon,
-    /* Text - Titel, "%s" ersätts med det dekorerades namn. */
-    title: scriptVariables.title
+  /* Text - Klass för omgivande <div>. Om ej angiven skapas ingen div. */
+  wrapperClass: scriptVariables.wrapperClass,
+  /* Text - Fontawesomeikon, t ex "fa-comments". */
+  icon: scriptVariables.icon,
+  /* Text - Titel, "%s" ersätts med det dekorerades namn. */
+  title: scriptVariables.title
 };
 
 // Returnerar noden som är dekorationsinnehållet.
 var getContentNode = function() {
-   var portletId = request.getContext().getCurrentContent();
-   return ResourceLocatorUtil.getNodeByIdentifier(portletId);
+  var portletId = request.getContext().getCurrentContent();
+  return ResourceLocatorUtil.getNodeByIdentifier(portletId);
 };
 
 try {
-   var contentNode = getContentNode();
-
-   if (contentNode) {
-      var contentId = contentNode.getIdentifier().replace(".", "_"),
-          contentName = PropertyUtil.getString(contentNode, 'displayName');
-   }
-   if (interface.title && interface.title !== '') {
-        contentName = interface.title.replace("%s", contentName || "").trim();
-   }
+  var contentNode = getContentNode();
+  var currentPageId = PortletContextUtil.getCurrentPage().getIdentifier();
+  if (contentNode) {
+    var contentId = contentNode.getIdentifier().replace(".", "_"),
+      contentName = PropertyUtil.getString(contentNode, 'displayName');
+  }
+  if (interface.title && interface.title !== '') {
+    contentName = interface.title.replace("%s", contentName || "").trim();
+  }
 } catch (e) {
-   out.println("Antingen är sidan som är dekorerad felaktig eller så är kan inte sidan visas i detta läge: " + e);
+  out.println("Antingen är sidan som är dekorerad felaktig eller så är kan inte sidan visas i detta läge: " + e);
 }
